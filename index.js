2f1d852cd4cf01e145070a86ac8b93c4dabaa878
chida09 <t.chida0909@gmail.com>
2022-12-31 22:47:59 +0900

    実装追加

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const sessionOption = {
   // 推奨項目（新規にセッションを生成して何も代入されていなくても値を入れる場合にはtrue）
   saveUninitialized: true,
   // 生存期間（単位：ミリ秒）
-  cookie: { maxAge: 60 * 60 * 1000 },
+  cookie: {maxAge: 60 * 60 * 1000},
   // クッキー名（デフォルトでは「connect.sid」）
-  name : 'my-site-cookie',
+  name: 'my-site-cookie',
   // アクセスの度に、有効期限を伸ばす場合にはtrue
-  rolling : true,
+  rolling: true,
 }
 app.use(session(sessionOption))
 app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
 const connect = mysql.createConnection({
@@ -33,44 +33,61 @@ const connect = mysql.createConnection({
   port: 3308
 })
 
-// ユーザー一覧表示
+// View - ユーザー一覧
 app.get('/', (req, res) => {
   const sql = "select * from users"
   connect.query(sql, function (err, result, fields) {
     if (err) throw err
-    res.render('index',{users : result})
+    res.render('index', {users: result})
   })
 })
 
+// View - ユーザー情報
 app.get('/member', (req, res) => {
+  // const sql = "select * from users where name"
+  // connect.query(sql, function (err, result, fields) {
+  //   if (err) throw err
   res.render('member', {
     session: req.session.user.name,
-    cookies: req.cookies['my-site-cookie']
+    cookies: req.cookies['my-site-cookie'],
+    id: 1
   })
+  // })
 })
 
-// 新規アカウント作成
+// API - 新規アカウント作成
 app.post('/', (req, res) => {
   const sql = "INSERT INTO users SET ?"
-  connect.query(sql,req.body,function(err, result, fields){
+  connect.query(sql, req.body, function (err, result, fields) {
     if (err) throw err
     res.redirect('/')
   })
 })
 
-// ログイン
+// API - ログイン
 app.post('/login', (req, res) => {
-  // const sql = "SELECT * FROM users WHERE name = ? AND password = ?"
+  // const sql = "SELECT * FROM users WHERE name = " + req.params.id
   // connect.query(sql,req.body.name, req.body.password,function(err, result, fields){
   //   if (err) throw err
-
   // セッションに保存する
-  req.session.user = req.session.user || { name: req.body.name }
+  req.session.user = req.session.user || {name: req.body.name}
   res.redirect('/member')
   // })
 })
 
-// ログアウト
+app.post('/user', (req, res) => {
+  console.log(req.body)
+  const sql = "UPDATE users SET name = ? WHERE id = ?"
+  connect.query(sql, [req.body.name, req.body.id], function (err, result, fields) {
+
+    if (err) throw err
+    req.session.user.name = req.body.name
+    res.redirect('/member')
+  })
+
+})
+
+// API - ログアウト
 app.get('/logout', (req, res) => {
   req.session.destroy()
   res.redirect('/')
--- initdb.d/sample.sql
+++ initdb.d/sample.sql
@@ -1,12 +1,15 @@
 DROP TABLE IF EXISTS `users`;
 
+-- エディタ上に出ているエラーは無視で良い
 CREATE TABLE users
 (
+    id         int(11) NOT NULL AUTO_INCREMENT,
     name         varchar(60),
     mail_address text,
     password     varchar(100),
-    created_at   datetime default current_timestamp
-);
+    created_at   datetime default current_timestamp,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO users (name, mail_address, password)
 VALUES ('yamada', 'yamada@test.com', '123');
